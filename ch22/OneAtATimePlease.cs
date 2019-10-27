91115e65234ef70d94731cca45b5843752f16692
Eric Johannsen <eric@johannsen.us>
2019-10-27 14:30:53 -0700

    Mutex and Global

--- ch22/OneAtATimePlease.cs
+++ ch22/OneAtATimePlease.cs
@@ -8,7 +8,7 @@ public class OneAtATimePlease
     // Naming a Mutex makes it available computer-wide. Use a name that's
     // unique to your company and application (e.g., include your URL).
 
-    using var mutex = new Mutex (true, "oreilly.com OneAtATimeDemo");
+    using var mutex = new Mutex (true, @"Global\oreilly.com OneAtATimeDemo");
     // Wait a few seconds if contended, in case another instance
     // of the program is still in the process of shutting down.
 
